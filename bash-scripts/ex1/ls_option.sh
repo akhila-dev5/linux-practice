04662dfb355ff0a03411f7e707a9eaad5d313e97
akhila-dev5 <gurujuakhila.dev@gmail.com>
2025-12-01 15:50:17 +0000

    ls cmd

---
+++ bash-scripts/ex1/ls_option.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+set -x
+
+#default order: alphabetical by name
+# -l for long format
+ls -l
+
+# -r reverses the sort order
+ls -lr
+
+# -t modification time (newest by default)
+ls -lt
+
+# -t 
+ls -lrt
