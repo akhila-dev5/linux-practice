29f95d80fb4d44dff2f582bd12b880e3b388153c
akhila-dev5 <gurujuakhila.dev@gmail.com>
2025-12-01 15:02:23 +0000

    example of cmd substitute

---
+++ bash-scripts/ex1/cmd_subs.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+########################################
+#Author:    Akhila
+#Date:      01 Dec 2025
+#Subject:   Script to display date and time using cmd substitution
+########################################
+
+current_datetime=$(date)
+
+echo "Current date and time is $current_datetime "
