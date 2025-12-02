d35c99be186dd9ee18c0343836debf72e0bab5f3
akhila-dev5 <gurujuakhila.dev@gmail.com>
2025-12-02 15:30:55 +0000

    interpolation & numeric

---
+++ bash-scripts/var-declaration-usage/interpolation.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+#variable interpolation
+
+firstName="Akhila"
+secondName="Guruju"
+
+echo "Hello ${firstName} ${secondName}, Welcome!"
---
+++ bash-scripts/var-declaration-usage/numeric.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+#Numeric Variables
+
+name="Pavan"
+age=21
+
+echo "$name's age is $age"
