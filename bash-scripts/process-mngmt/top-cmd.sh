91902a4fe283b46b907dd127c8fb934edf17aa08
akhila-dev5 <gurujuakhila.dev@gmail.com>
2025-12-30 15:16:43 +0000

    script for top cmd

---
+++ bash-scripts/process-mngmt/top-cmd.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# ps is static
+# top is dynamic (real-time process monitor.)
+
+##############################
+# Key interactions
+
+# Press M to sort by memory usage
+# Press P to sort by CPU usage
+# Press k followed by a PID to kill a process directly from top
+# Press 1 to see individual CPU cores (useful for multi-core servers)
+# Press u followed by a user name to see only input user process
+###############################
+
+top
+
+
