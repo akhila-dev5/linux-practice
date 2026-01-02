dc5713cf921384be2e296da842a3883fd29289ca
akhila-dev5 <gurujuakhila.dev@gmail.com>
2026-01-02 15:24:23 +0000

    modified script

--- bash-scripts/linking/symlink.sh
+++ bash-scripts/linking/symlink.sh
@@ -4,4 +4,9 @@ echo "Content can be displayed using symbolic link" > var/www/data.txt
 
 ln -s var/www/data.txt data
 
-echo "Symbolic link is data"
+if [[ -L "data" ]]; then
+	echo "Symbolic or soft link is created"
+	echo "Symbolic link is data"
+else
+	echo "Creation of symbolic or soft link is failed"
+fi
