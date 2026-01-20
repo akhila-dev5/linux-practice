b8c38cd5a4867f951789d39f91a2dfde8cf1fbf5
akhila-dev5 <gurujuakhila.dev@gmail.com>
2026-01-20 12:14:02 +0000

    extend power func for neg exponents

--- bash-scripts/functions/power-func.sh
+++ bash-scripts/functions/power-func.sh
@@ -3,13 +3,19 @@
 power() {
 	local base=$1
 	local exp=$2
+	local res
 
 	if [[ $base -eq 0 && $exp == 0 ]]; then
 		echo "Exceptioal Error! 0 power of 0 is not possible."
 		return
 	fi
 
-	local res=$((base ** exp))
+	if [[ $exp -lt 0 ]]; then
+		res=$(bc <<< "scale=5; 1/($base ^ (-($exp)))")
+	else
+		res=$((base ** exp))
+	fi
+
 	echo "$base to the power of $exp is $res"
 }
 
@@ -17,5 +23,6 @@ power 2 2
 power 7 2
 power -2 3
 power 0 0
+power 2 -1
 
 
